ea7c7ee70b8684fde7ed466401c4596adedf2e3d
twoheart <twoheart@vip.qq.com>
2019-07-04 11:45:34 +0800

    build

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -276,7 +276,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63337" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53918" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -452,4 +452,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../usr/local/lib/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
